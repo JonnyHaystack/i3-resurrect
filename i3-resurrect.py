58808e54effac317d15665b62dee279c43ce8db0
Jonathan Haylett <jonathan@haylett.dev>
2019-06-30 22:32:28 +0100

    Get command saving working

--- i3-resurrect.py
+++ i3-resurrect.py
@@ -1,10 +1,7 @@
 import errno
+import os
+import subprocess
 import sys
-from os import makedirs
-from os.path import exists as path_exists
-from os.path import expanduser
-from os.path import join as join_path
-from subprocess import getoutput
 
 import click
 import i3ipc
@@ -38,91 +35,127 @@ def main():
 @click.option('--workspace', '-w', help='The workspace to save')
 @click.option('--directory', '-d',
               type=click.Path(file_okay=False, writable=True),
-              default=expanduser('~/.i3/i3-resurrect/'),
+              default=os.path.expanduser('~/.i3/i3-resurrect/'),
               help='Directory to use for saving/restoring workspaces')
 def save(workspace, directory):
+    """
+    Saves an i3 workspace's layout and commands to launch open programs to a
+    file.
+    """
     # Create directory if non-existent.
-    if not path_exists(directory):
+    if not os.path.exists(directory):
         try:
-            makedirs(directory)
+            os.makedirs(directory)
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
+
     # Save workspace layout to file.
-    json_tree = getoutput(f'i3-save-tree --workspace {workspace}')
+    save_layout(workspace, directory)
+
+    # Save commands to file.
+    save_commands(workspace, directory)
+
+
+def save_layout(workspace, directory):
+    """
+    Saves an i3 workspace layout to a file.
+    """
+    json_tree = subprocess.getoutput(f'i3-save-tree --workspace {workspace}')
     json_lines = iter(json_tree.splitlines())
-    with open(join_path(directory, f'workspace_{workspace}.json'), 'w') as f:
-        # Strip out the comments that i3-save-tree includes, and remove unwanted
-        # swallow criteria.
+    with open(os.path.join(
+            directory, f'workspace_{workspace}.json'), 'w') as file:
+        # Strip out the comments that i3-save-tree includes, and remove
+        # unwanted swallow criteria.
         for line in json_lines:
             if line.strip().startswith('// '):
                 if 'class' in line:
-                    print(line.replace('// ', '   ', 1))
+                    processed_line = line.replace('// ', '   ', 1)
+                    file.write(f'{processed_line}\n')
                 elif 'instance' in line:
-                    print(line.replace('// ', '   ', 1)[:-1])
+                    processed_line = line.replace('// ', '   ', 1)[:-1]
+                    file.write(f'{processed_line}\n')
             else:
-                print(line)
+                file.write(f'{line}\n')
 
-    i3 = i3ipc.Connection()
 
-    # Loop through windows and save commands to launch programs on saved
-    # workspace.
-    for con in i3.get_tree():
-        if (not con.window
-                or con.parent.type == 'dockarea'
-                or con.workspace().name != sys.argv[1]):
-            continue
+def save_commands(workspace, directory):
+    """
+    Saves the commands to launch the programs open in the specified workspace
+    to a file.
+    """
+    with open(os.path.join(
+            directory, f'workspace_{workspace}.sh'), 'w') as file:
+        # Write hashbang to start of file.
+        file.write('#!/usr/bin/env bash')
 
-        # Get information on this window.
-        try:
-            window = Window.by_id(con.window)[0]
-        except ValueError:
-            continue
-        try:
-            window
-        except NameError:
-            continue
-        if not window:
-            continue
-        pid = window.pid
-        if pid == 0:
-            continue
+        i3 = i3ipc.Connection()
 
-        # Get process info for the window.
-        procinfo = psutil.Process(pid)
+        # Loop through windows and save commands to launch programs on saved
+        # workspace.
+        for con in i3.get_tree():
+            if (not con.window
+                    or con.parent.type == 'dockarea'
+                    or con.workspace().name != workspace):
+                continue
 
-        try:
-            working_directory = procinfo.cwd()
-        except Exception:
-            working_directory = '$HOME'
+            # Get information on the window.
+            try:
+                window = Window.by_id(con.window)[0]
+            except ValueError as e:
+                eprint(str(e))
+                continue
 
-        # If the program is a terminal, get the working directory from the
-        # window title.
-        if con.window_class in TERMINALS:
-            # Remove any non-ASCII characters.
-            working_directory = str(con.name.encode('ascii', 'ignore').strip())
+            # Pre-emptively attempt to catch error
+            try:
+                window
+            except NameError as e:
+                eprint(str(e))
+                continue
 
-        # Change ~ to $HOME.
-        working_directory = working_directory.replace('~', '$HOME')
+            if not window:
+                continue
+            pid = window.pid
+            if pid == 0:
+                continue
 
-        # Create command to launch program.
-        # If there is a special command mapping for this program, use that.
-        if con.window_class in window_class_command_mappings:
-            command = '(cd {0}; {1} &)'.format(
-                working_directory,
-                window_class_command_mappings[con.window_class],
-            )
-        else:
-            # If the program has no special mapping, launch it by cd'ing to its
-            # working directory (obtained by psutil) and then executing the
-            # first index of the cmdline (hopefully this will work for almost
-            # all programs I use at least).
-            command = '(cd {0}; {1} &)'.format(
-                working_directory,
-                procinfo.cmdline()[0],
-            )
-        # Print the command.
-        print('\n{0}'.format(command))
+            # Get process info for the window.
+            procinfo = psutil.Process(pid)
+
+            try:
+                working_directory = procinfo.cwd()
+            except Exception:
+                working_directory = '$HOME'
+
+            # If the program is a terminal, get the working directory from the
+            # window title.
+            if con.window_class in TERMINALS:
+                # Remove any non-ASCII characters.
+                working_directory = str(con.name
+                                        .encode('ascii', 'ignore')
+                                        .strip())
+
+            # Change ~ to $HOME.
+            working_directory = working_directory.replace('~', '$HOME')
+
+            # Create command to launch program.
+            # If there is a special command mapping for this program, use that.
+            if con.window_class in window_class_command_mappings:
+                command = '(cd {0}; {1} &)'.format(
+                    working_directory,
+                    window_class_command_mappings[con.window_class],
+                )
+            else:
+                # If the program has no special mapping, launch it by cd'ing to
+                # its working directory (obtained by psutil) and then executing
+                # the first index of the cmdline (this works for almost all
+                # programs I use at least).
+                command = '(cd {0}; {1} &)'.format(
+                    working_directory,
+                    procinfo.cmdline()[0],
+                )
+            # Print the command.
+            file.write(f'\n\n{command}')
 
 
 if __name__ == '__main__':
