c8a807ec75a5898c4e44424b25be5abc2ac49de3
Jonathan Haylett <jonathan@haylett.dev>
2019-07-09 14:04:36 +0100

    Update version to 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='i3-resurrect',
-    version='1.0.3',
+    version='1.0.4',
     author='Jonathan Haylett',
     author_email='jonathan@haylett.dev',
     py_modules=['i3_resurrect'],
