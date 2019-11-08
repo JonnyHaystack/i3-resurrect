6c8f77316b8a5f80c46dea8c734e7edd725a4ca7
Jonathan Haylett <jonathan@haylett.dev>
2019-11-08 01:08:45 +0000

    Update __init__.py files

--- i3_resurrect/__init__.py
+++ i3_resurrect/__init__.py
@@ -1,5 +1,8 @@
-__all__ = ['config', 'main', 'util']
+__all__ = ['config', 'layout', 'main', 'programs', 'tree', 'util']
 
 from . import config
+from . import layout
 from . import main
+from . import programs
+from . import tree
 from . import util
