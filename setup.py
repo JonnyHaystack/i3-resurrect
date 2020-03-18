ec33bdeef63db84e90ee5ec8743308481dc05977
Jonathan Haylett <jonathan@haylett.dev>
2020-03-18 13:13:20 +0000

    Prepare release 1.4.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='i3-resurrect',
-    version='1.4.2',
+    version='1.4.3',
     packages=['i3_resurrect'],
     author='Jonathan Haylett',
     author_email='jonathan@haylett.dev',
