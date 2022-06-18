85a5e76697e9283ced5d5c66c49da943b4cd5219
Jonathan Haylett <jonathan@haylett.dev>
2022-06-18 18:43:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='i3-resurrect',
-    version='1.4.3',
+    version='1.4.5',
     packages=['i3_resurrect'],
     author='Jonathan Haylett',
     author_email='jonathan@haylett.dev',
