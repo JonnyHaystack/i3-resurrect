f564012143b3cf3f39a6c3043ba502ad48161116
jim <jim@v-max.ca>
2024-04-29 14:02:46 -0700

    bump version to 1.4.5.a

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='i3-resurrect',
-    version='1.4.5',
+    version='1.4.5.a',
     packages=['i3_resurrect'],
     author='Jonathan Haylett',
     author_email='jonathan@haylett.dev',
